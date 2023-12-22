a9f5b130d6a99a687ca6a261e3930c7bb50f2fa9
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2023-12-22 22:37:15 +0900

    Update dataProcessing.js

--- dataProcessing.js
+++ dataProcessing.js
@@ -1,6 +1,14 @@
 function initialAssignStudents(data, numClasses) {
-    // 현재는 입력값 그대로 반환합니다. 실제 로직 구현 시 수정 필요
-    return data;
+    // 임시로 학생들을 반별로 분배하는 로직
+    const studentsPerClass = Math.ceil(data.length / numClasses);
+    let classAssignments = Array.from({ length: numClasses }, () => []);
+
+    data.forEach((student, index) => {
+        const classIndex = index % numClasses;
+        classAssignments[classIndex].push(student);
+    });
+
+    return classAssignments;
 }
 
 function minimizeSameClassStudents(classAssignments) {
