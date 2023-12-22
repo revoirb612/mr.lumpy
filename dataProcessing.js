39954f810c568b6b4db39472dd3f55cd18d14cd6
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2023-12-22 21:05:52 +0900

    Update and rename script.js to dataProcessing.js

---
+++ dataProcessing.js
@@ -0,0 +1,19 @@
+function initialAssignStudents(data, numClasses) {
+    // 현재는 입력값 그대로 반환합니다. 실제 로직 구현 시 수정 필요
+    return data;
+}
+
+function minimizeSameClassStudents(classAssignments) {
+    // 현재는 입력값 그대로 반환합니다. 실제 로직 구현 시 수정 필요
+    return classAssignments;
+}
+
+function maximizeScoreDiversity(classAssignments) {
+    // 현재는 입력값 그대로 반환합니다. 실제 로직 구현 시 수정 필요
+    return classAssignments;
+}
+
+function balanceGuidanceDifficulty(classAssignments) {
+    // 현재는 입력값 그대로 반환합니다. 실제 로직 구현 시 수정 필요
+    return classAssignments;
+}
