511a4d52258fd27dac406e6cd499484dbcf8ff72
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2023-12-24 11:11:03 +0900

    Update dataAnalysis.js

--- dataAnalysis.js
+++ dataAnalysis.js
@@ -16,3 +16,52 @@ function analyzeUniqueDataCount(data) {
 
     return uniqueCounts;
 }
+
+function calculateGroupVariance(data, groupHeader) {
+    // 그룹별로 데이터를 저장할 객체
+    let groups = {};
+
+    // 데이터를 그룹화
+    data.forEach(row => {
+        let key = row[groupHeader];
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+        groups[key].push(row);
+    });
+
+    // 각 그룹별 분산 계산
+    let variances = {};
+    for (let key in groups) {
+        let group = groups[key];
+        let sum = 0;
+        let count = 0;
+
+        // 평균 계산
+        group.forEach(row => {
+            Object.keys(row).forEach(header => {
+                if (!isNaN(row[header])) {
+                    sum += parseFloat(row[header]);
+                    count++;
+                }
+            });
+        });
+
+        let mean = sum / count;
+
+        // 분산 계산
+        let varianceSum = 0;
+        group.forEach(row => {
+            Object.keys(row).forEach(header => {
+                if (!isNaN(row[header])) {
+                    let value = parseFloat(row[header]);
+                    varianceSum += (value - mean) ** 2;
+                }
+            });
+        });
+
+        variances[key] = varianceSum / count;
+    }
+
+    return variances;
+}
