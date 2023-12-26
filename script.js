5bfc77d2818dac98275567a41b2c62a4307193d4
홍쌤 <54047794+revoirb612@users.noreply.github.com>
2023-12-26 11:25:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -180,9 +180,6 @@ function calculateGroupDataCounts() {
 
 // 테이블 생성 함수
 function createGroupTable(group, groupId) {
-    // ID 열을 기준으로 데이터를 오름차순으로 정렬
-    group.sort((a, b) => a.ID - b.ID);
-
     let table = document.createElement('table');
     table.border = '1';
     
